94a572d757fcc1b47771bafc67d97df84f484d96
LucioWi <luciowiesek@gmail.com>
2024-09-26 00:09:17 -0300

    Actualización
    funcionalidad a los botones principalmente

--- projects/hola-mundo/src/App.jsx
+++ projects/hola-mundo/src/App.jsx
@@ -3,9 +3,11 @@ import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 export function App() {
     return (
         <section className="TwitterUsersFollow">
-        <TwitterFollowCard username="@arroba" name="Nombre de usuario" isFollowing/> {/*Si el valor de un booleano es true no hace falta poner el "={true}".*/}
-        <TwitterFollowCard username="@BocaJrsOficial" name="Boca Juniors" isFollowing={false}/>
-        <TwitterFollowCard username="@DavooXeneizeJRR" name="Davoo Xeneize" isFollowing={false}/>
+        <TwitterFollowCard username="@arroba" name="Nombre de usuario" initialisFollowing/>
+        <TwitterFollowCard username="@BocaJrsOficial" name="Boca Juniors" initialisFollowing={false}/>
+        <TwitterFollowCard username="@DavooXeneizeJRR" name="Davoo Xeneize" initialisFollowing={false}/>
         </section> // El <> es utilizado para mostrar más de un componente a la vez. Puede ser cualquier cosa realmente, como en este caso un section.
     )
 }
+
+//Si el valor de un booleano es true no hace falta poner el "boolean={true}".
--- projects/hola-mundo/src/TwitterFollowCard.jsx
+++ projects/hola-mundo/src/TwitterFollowCard.jsx
@@ -1,5 +1,12 @@
-export function TwitterFollowCard({ username, name, isFollowing }) {
+import { useState } from "react"
+export function TwitterFollowCard({ username, name, initialisFollowing}) { //username, name e isFollowing son PROPS, una vez creadas no se deben modificar mas adelante en el código.
+    const [isFollowing, setIsFollowing] = useState(initialisFollowing)
     const imgSrc = `https://unavatar.io/x/${username}`
+    const text = isFollowing ? 'Siguiendo' : 'Seguir' //esto es basicamente un if
+    const buttonClassName = isFollowing ? 'tw-followCard-button is-following' : 'tw-followCard-button'
+    const handleClick = () => {
+        setIsFollowing(!isFollowing)
+    }
     return (
         <article className="tw-followCard"/* Para crear una clase se debe usar className en vez de class ya que este mismo se usa para algo aparte en js*/>
             <header className="tw-followCard-header">
@@ -10,7 +17,7 @@ export function TwitterFollowCard({ username, name, isFollowing }) {
                 </div>
             </header>
             <aside>
-                <button className="tw-followCard-button">{isFollowing}</button>
+                <button className={buttonClassName} onClick={handleClick}>{text}</button>
             </aside>
         </article>
     )
